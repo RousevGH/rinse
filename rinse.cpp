2af34751828bb1f16328417fd3da5f078dfb5f1c
FarofaCodez <anaj42863@outlook.com>
2026-01-18 22:10:36 +0000

    idk 2

--- rinse.cpp
+++ rinse.cpp
@@ -636,7 +636,7 @@ void install_packages(const std::vector<std::string>& pkgs) {
         }
         std::cout << CYAN << "\nInstalling from Flatpak..." << RESET << std::endl;
         for (const auto& pkg : flatpak_pkgs) {
-            std::string cmd = "flatpak install -y flathub " + sanitize_package(pkg) + " --system";
+            std::string cmd = "flatpak install -y flathub " + sanitize_package(pkg);
             show_progress(cmd, "Installing");
         }
     }
